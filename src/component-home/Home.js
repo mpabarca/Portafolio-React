fa65eb3cdaf1ac3c40691d1036bc743900e78a55
mpabarca <mpabarca@uc.cl>
2019-03-24 11:56:52 -0300

    se agrega navbar no responsive

--- src/component-home/Home.js
+++ src/component-home/Home.js
@@ -6,7 +6,18 @@ class Home extends Component{
     render(){
         return(
             <div className="Home" id="home">
-                
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#" className="brand-logo">LOGO</a>
+                        <ul className="right hide-on-med-and-down">
+                            <li><a href="">HOME</a></li>
+                            <li><a href="">ABOUT ME</a></li>
+                            <li><a href="">SKILLS</a></li>
+                            <li><a href="">PROJECTS</a></li>
+                            <li><a href="">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
             
         )
