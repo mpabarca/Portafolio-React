e90fbbbe27894004f181c13cc20ce1c34a257b9f
mpabarca <mpabarca@uc.cl>
2019-04-02 11:33:23 -0300

    diseño basico skills listo

--- src/component-navbar/Navbar.js
+++ src/component-navbar/Navbar.js
@@ -6,30 +6,30 @@ class Navbar extends Component{
     render(){
         return(
             <div>
-                <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-                    <a className="navbar-brand" href="#">LOGO</a>
+                <nav id="Navbar" className="navbar navbar-expand-md navbar-dark">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="collapsibleNavbar">
                         <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About me</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Skills</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Projects</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>    
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">HOME</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">ABOUT ME</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">SKILLS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">PROJECTS</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">CONTACT</a>
+                            </li>    
                         </ul>
                     </div>  
+                    <a className="navbar-brand" href="#">LOGO</a>
                 </nav>
             </div>
         )
--- src/component-skills/Skills.js
+++ src/component-skills/Skills.js
@@ -2,6 +2,29 @@ import React, {Component} from 'react';
 import './skills.css';
 import { Container, Row, Col, Button, Image } from 'react-bootstrap';
 
+const skillsButton = {
+    background:'#7a2828',
+    border: 'none',
+    borderRadius: 0,
+    fontFamily: "'Merriweather', serif",
+    fontWeight:'600',
+    height: 'auto',
+    marginTop: '10%'
+  };
+
+const skillsText = {
+    fontFamily: "'Roboto', sans-serif",
+    fontWeight: '300',
+    fontSize: '25px'
+};
+const skillsIcon = {
+    fontSize:'500%'
+  };
+const skillsGroupIcon = {
+    marginTop: '5%'
+};
+
+
 class Skills extends Component{
 
     render(){
@@ -10,8 +33,8 @@ class Skills extends Component{
                 <Container>
                     <Row>
                         <Col>
-                            <Row>skills</Row>
-                            <Row>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                            <Row id="skills-tittle">skills</Row>
+                            <Row tyle={skillsText}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                                 sed do eiusmod tempor incididunt ut labore et dolore magna 
                                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
                                 ullamco laboris nisi ut aliquip ex ea commodo consequat. 
@@ -21,26 +44,35 @@ class Skills extends Component{
                                 deserunt mollit anim id est laborum."
                             </Row>
                             <Row>
-                                <Col><Button>Sp'</Button></Col>
-                                <Col><Button>En'</Button></Col>
+                                <Col><Button style={skillsButton}>Sp'</Button></Col>
+                                <Col><Button style={skillsButton}>En'</Button></Col>
                             </Row>
                         </Col>
-                        <Col>
-                            <i class="devicon-css3-plain-wordmark"></i>
-                            <i class="devicon-git-plain-wordmark"></i>
-                            <i class="devicon-github-plain-wordmark"></i>
-                            <i class="devicon-html5-plain-wordmark"></i>
-                            <i class="devicon-javascript-plain"></i>
-                            <i class="devicon-nodejs-plain-wordmark"></i>
-                            <i class="devicon-python-plain-wordmark"></i>
-                            <i class="devicon-react-original-wordmark"></i>
-                            <i class="devicon-trello-plain-wordmark"></i>
-                            <i class="devicon-visualstudio-plain-wordmark"></i>
-                            <i class="devicon-mocha-plain"></i>
-                            <i class="devicon-ubuntu-plain-wordmark"></i>
-                            <i class="devicon-bootstrap-plain-wordmark"></i>
-                            
-
+                        <Col style={skillsGroupIcon}>
+                            <Row>
+                                <Col><i style={skillsIcon} class="devicon-html5-plain-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-javascript-plain"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-react-original-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-python-plain-wordmark"></i></Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col><i style={skillsIcon} class="devicon-css3-plain-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-bootstrap-plain-wordmark"></i></Col>
+                                <Col></Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col><i style={skillsIcon} class="devicon-git-plain-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-github-plain-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-nodejs-plain-wordmark"></i></Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col><i style={skillsIcon} class="devicon-ubuntu-plain-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-visualstudio-plain-wordmark"></i></Col>
+                                <Col><i style={skillsIcon} class="devicon-trello-plain-wordmark"></i></Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Container>
